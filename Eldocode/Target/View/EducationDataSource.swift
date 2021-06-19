693880f0566383c2b6a3f16bd0b2715c51e2af7a
Ivan Kopiev <kopiev@mustdev.ru>
2021-06-19 23:27:54 +0300

    upd educationDataSource

--- Eldocode/Target/View/EducationDataSource.swift
+++ Eldocode/Target/View/EducationDataSource.swift
@@ -15,7 +15,15 @@ class EducationDataSource:NSObject, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CustomButton.id, for: indexPath) as? CustomButton else { return UICollectionViewCell()}
-        
+        if indexPath.row == 1 {
+            cell.logoImageView.image = UIImage(named: "rating")
+            cell.textLabel.text = "Как повысить\nсредний чек\nпокупателя"
+        }
+        if indexPath.row == 2 {
+            cell.logoImageView.image = UIImage(named: "rating")
+            cell.textLabel.text = "Улучши\nобщение\nс клиентами"
+        }
+
         
         return cell
     }
