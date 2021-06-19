9930175fb036a2a8ed48fc904d3c0bea18eba60c
Ivan Kopiev <kopiev@mustdev.ru>
2021-06-19 20:00:21 +0300

    Initial Commit

---
+++ Eldocode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Eldocode
+//
+//  Created by Ivan Kopiev on 19.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
