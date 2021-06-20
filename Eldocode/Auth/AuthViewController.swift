02ca3715fde1e80bd630fbe4c37a68536b5d290f
Ivan Kopiev <kopiev@mustdev.ru>
2021-06-20 07:43:18 +0300

    Create AuthViewController

---
+++ Eldocode/Auth/AuthViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AuthViewController.swift
+//  Eldocode
+//
+//  Created by Ivan Kopiev on 20.06.2021.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
