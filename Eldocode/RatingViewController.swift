716c3d6f98633a3293a5346365f193d030edf27a
Ivan Kopiev <kopiev@mustdev.ru>
2021-06-20 07:45:15 +0300

    Create RatingViewController

---
+++ Eldocode/RatingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RatingViewController.swift
+//  Eldocode
+//
+//  Created by Ivan Kopiev on 20.06.2021.
+//
+
+import UIKit
+
+class RatingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
